f8f79d9fd134a3a4b768f364720ff9944d5e2506
emrea-works <gelisimodakli@gmail.com>
2022-12-07 11:57:05 -0300

    express app functions til views

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
 /** @params
@@ -8,6 +10,19 @@ app.listen(3000);
  */
 app.get('/', (req, res) => {
   console.log('here');
+  // Send status by reserved http status codes
+  //res.sendStatus(500);
   // Dispatch message to viewer
-  res.send('Hi');
+  //res.send('Hi');
+  // Send status with a message
+  //res.status(500).send('Server down (Error code: 500)');
+  // Send status with a json
+  //res.status(500).json({ message: 'Server down (Error code: 500)' });
+  // Download a file
+  //res.download('server.js');
+  // Render a file to client
+  /**
+   * requires view engine, like pug or ejs
+   */
+  res.render('index');
 });
