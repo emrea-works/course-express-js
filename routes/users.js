0e2f2717e13fd151c6c7e1c8f3e27c215a822623
emrea-works <gelisimodakli@gmail.com>
2022-12-11 15:45:47 -0300

    router: cleaner code by chaining the router functions

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,34 @@ router.post('/', (req, res) => {
   res.send('Create User');
 });
 
+/** 
+ * Routers below are same, in order to write compact code, 
+ * we are going to user router's function piplining feature
+ */
+/*
 router.get('/:id', (req, res) => {
   res.send(`Get user with id ${req.params.id}`);
 });
+
+router.put('/:id', (req, res) => {
+  res.send(`Update user with id ${req.params.id}`);
+});
+
+router.delete('/:id', (req, res) => {
+  res.send(`Delete user with id ${req.params.id}`);
+});
+*/
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Get user with id ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update user with id ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete user with id ${req.params.id}`);
+  });
 
 module.exports = router;
