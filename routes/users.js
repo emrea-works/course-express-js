98d40f380c9f2fbfe2ccc613fe3eb70e3c74f93b
emrea-works <gelisimodakli@gmail.com>
2022-12-11 17:01:14 -0300

    parsing form: create form and setting up the router

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.use(logger)
-
-router.get('/', (req, res) => {
-  res.send('User List');
+router.get("/", (req, res) => {
+  res.send("User List");
 });
 
-router.get('/new', (req, res) => {
-  res.send('New User Form');
+router.get("/new", (req, res) => {
+  // second parameter enters "test" text inside the input box
+  res.render("users/new", { firstName: "test" });
 });
 
-router.post('/', (req, res) => {
-  res.send('Create User');
+router.post("/", (req, res) => {
+  res.send("Create User");
 });
 
 router
@@ -31,12 +30,6 @@ const users = [{ name: "Alice" }, { name: "Bob" }];
 router.param("id", (req, res, next, id) => {
   req.user = users[id];
   next();
-});  
-
-function logger(req, res, next) {
-  console.log(req.originalUrl);
-  next();
-}
-
+});
 
 module.exports = router;
